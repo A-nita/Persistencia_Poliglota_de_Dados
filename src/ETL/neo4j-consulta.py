ba47d339d4603b10618b8f0e42bc2b8f02cb5c65
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-08-30 21:28:12 -0300

    commit

---
+++ src/ETL/neo4j-consulta.py
@@ -0,0 +1,35 @@
+from py2neo import Graph
+from tqdm import tqdm
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+
+def generate_recommendations():
+    results = graph.run(
+    '''
+    MATCH (user: User)
+    CALL {
+        WITH user
+        MATCH (user) -[user2Ratings:Rating]-> (likedUser2Animes: Anime) -[similarities:Similar]-> (similarAnimes:Anime)
+        RETURN similarAnimes.anime_id AS recomended_anime_id, similarAnimes.name AS recomended_anime_name, SUM(similarities.gosteis) AS gosteis
+        ORDER BY gosteis DESC
+        LIMIT 3
+    }
+    RETURN user.user_id AS target_user, recomended_anime_id, recomended_anime_name, gosteis
+    '''
+    )
+
+    results_list = results.data()
+
+    recomendations = {}
+
+    for rec in results_list:
+        if rec['target_user'] not in recomendations:
+            recomendations[rec['target_user']] = []
+            
+        recomendations[rec['target_user']].append({
+            'recomended_anime_id': rec['recomended_anime_id'],
+            'recomended_anime_name': rec['recomended_anime_name']
+        })
+    
+    return recomendations
+
