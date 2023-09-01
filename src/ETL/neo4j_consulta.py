db575b216e24962a658fe5374a4b2d004eae6041
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-09-01 12:43:27 -0300

    atualizacao consulta + img neo4j

--- src/ETL/neo4j_consulta.py
+++ src/ETL/neo4j_consulta.py
@@ -10,11 +10,11 @@ def generate_recommendations():
     CALL {
         WITH user
         MATCH (user) -[user2Ratings:Rating]-> (likedUser2Animes: Anime) -[similarities:Similar]-> (similarAnimes:Anime)
-        RETURN similarAnimes.anime_id AS recomended_anime_id, similarAnimes.name AS recomended_anime_name, SUM(similarities.gosteis) AS gosteis
+        RETURN similarAnimes.anime_id AS recomended_anime_id, similarAnimes.name AS recomended_anime_name, similarAnimes.img AS recomended_anime_img, SUM(similarities.gosteis) AS gosteis
         ORDER BY gosteis DESC
         LIMIT 3
     }
-    RETURN user.user_id AS target_user, recomended_anime_id, recomended_anime_name, gosteis
+    RETURN user.user_id AS target_user, recomended_anime_id, recomended_anime_name, recomended_anime_img
     '''
     )
 
@@ -28,7 +28,8 @@ def generate_recommendations():
             
         recomendations[rec['target_user']].append({
             'id': rec['recomended_anime_id'],
-            'name': rec['recomended_anime_name']
+            'name': rec['recomended_anime_name'],
+            'img': rec['recomended_anime_img']
         })
     
     return recomendations
